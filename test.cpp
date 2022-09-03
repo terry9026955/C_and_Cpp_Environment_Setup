bceaa5a03f192ab38c7be41a3dabee7f3240e7aa
terry <terry9026955@gmail.com>
2022-09-03 16:54:59 +0800

    clangd setting

--- test.cpp
+++ test.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
+void call(int e, int f){
+    cout << e << "  " << f << endl;
+    cout << "123";
+}
+
 int main(){
     int a = 8;
     int b = 7;
-    std::cout << "Hello world!\n";
-    std::cout << a << " " << b;
+    //int c;
+    cout << "Hello world!!!!\n";
+    std::cout << a << " " << b << endl;
+    call(5, 6);
+    return 0;
 }
+

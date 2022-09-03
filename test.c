6d6df678e089d3b6637a1100ee25a5ec1377aaa1
terry <terry9026955@gmail.com>
2022-09-03 12:29:01 +0800

    setting

---
+++ test.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main(void)
+{
+    int i = 10;
+    int j = 3;
+    printf("%d %d %d\n",j,i,j+i);
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int a = 8;
+    int b = 7;
+    std::cout << "Hello world!\n";
+    std::cout << a << " " << b;
+}
